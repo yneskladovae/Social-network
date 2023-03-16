49ccd7cc012bd3c32e415ce3dee52e383e16d902
yneskladovae <yneskladovae27@gmail.com>
2023-03-17 02:34:55 +0300

    added redux-store

---
+++ src/redux/redux-store.ts
@@ -0,0 +1,16 @@
+import {combineReducers, createStore} from "redux";
+import profileReducer from "./profile-reducer";
+import dialogsReducer from "./dialogs-reducer";
+import sidebarReducer from "./sidebar-reducer";
+import {StoreType} from "./store";
+
+let reducers = combineReducers({
+    profilePage: profileReducer,
+    dialogsPage: dialogsReducer,
+    sidebarFriends: sidebarReducer
+})
+
+export const store: StoreType = createStore(reducers)
+
+
+
