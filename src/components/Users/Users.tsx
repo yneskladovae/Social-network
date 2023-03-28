70d8b1b566108ebd2e0bc4e0f523dc53884d6dcd
yneskladovae <yneskladovae27@gmail.com>
2023-03-28 16:37:54 +0300

    lesson 52 - Side Effect

--- src/components/Users/Users.tsx
+++ src/components/Users/Users.tsx
@@ -13,67 +13,70 @@ type UsersPropsType = {
 
 export const Users: React.FC<UsersPropsType> = ({users, follow, unfollow, setUsers}) => {
 
-    if (users.length === 0) {
-        axios.get('https://social-network.samuraijs.com/api/1.0/users')
-            .then(response => {
-                setUsers(response.data.items)
-            })
-        // setUsers([
-        //     {
-        //         id: v1(),
-        //         fullName: 'Denis',
-        //         status: 'Lorem ipsum dolor sit amet',
-        //         location: {
-        //             city: 'Minsk',
-        //             country: 'Belarus',
-        //         },
-        //         isFollow: false,
-        //     },
-        //     {
-        //         id: v1(),
-        //         fullName: 'Stas',
-        //         status: 'Mymy',
-        //         location: {
-        //             city: 'Gomel',
-        //             country: 'Belarus',
-        //         },
-        //         isFollow: true,
-        //     },
-        //     {
-        //         id: v1(),
-        //         fullName: 'Tima',
-        //         status: 'viscas el barka',
-        //         location: {
-        //             city: 'barcelona',
-        //             country: 'Belarus',
-        //         },
-        //         isFollow: false,
-        //     },
-        //     {
-        //         id: v1(),
-        //         fullName: 'Anna',
-        //         status: 'mew mew',
-        //         location: {
-        //             city: 'Gomel',
-        //             country: 'Belarus',
-        //         },
-        //         isFollow: false,
-        //     },
-        //     {
-        //         id: v1(),
-        //         fullName: 'Crish',
-        //         status: 'Siiiiiiiiiiiii',
-        //         location: {
-        //             city: 'Minsk',
-        //             country: 'Belarus',
-        //         },
-        //         isFollow: true,
-        //     },
-        // ])
+    const getUsers = () => {
+        if (users.length === 0) {
+            axios.get('https://social-network.samuraijs.com/api/1.0/users')
+                .then(response => {
+                    setUsers(response.data.items)
+                })
+            // setUsers([
+            //     {
+            //         id: v1(),
+            //         fullName: 'Denis',
+            //         status: 'Lorem ipsum dolor sit amet',
+            //         location: {
+            //             city: 'Minsk',
+            //             country: 'Belarus',
+            //         },
+            //         isFollow: false,
+            //     },
+            //     {
+            //         id: v1(),
+            //         fullName: 'Stas',
+            //         status: 'Mymy',
+            //         location: {
+            //             city: 'Gomel',
+            //             country: 'Belarus',
+            //         },
+            //         isFollow: true,
+            //     },
+            //     {
+            //         id: v1(),
+            //         fullName: 'Tima',
+            //         status: 'viscas el barka',
+            //         location: {
+            //             city: 'barcelona',
+            //             country: 'Belarus',
+            //         },
+            //         isFollow: false,
+            //     },
+            //     {
+            //         id: v1(),
+            //         fullName: 'Anna',
+            //         status: 'mew mew',
+            //         location: {
+            //             city: 'Gomel',
+            //             country: 'Belarus',
+            //         },
+            //         isFollow: false,
+            //     },
+            //     {
+            //         id: v1(),
+            //         fullName: 'Crish',
+            //         status: 'Siiiiiiiiiiiii',
+            //         location: {
+            //             city: 'Minsk',
+            //             country: 'Belarus',
+            //         },
+            //         isFollow: true,
+            //     },
+            // ])
+        }
     }
-
+    
     return (
         <div>
+            <button onClick={getUsers}>Get Users</button>
             {users?.map(el => {
                 return (
                     <div key={el.id}>
